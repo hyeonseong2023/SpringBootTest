0b90aec1fd700703f1bc47557e38aab2cebb15ca
이현성 <528abcdef@naver.com>
2023-09-21 17:34:27 +0900

    서버 주소 변경

--- SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
+++ SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smhrd.player.service.PlayerService;
 
 @RestController // 리액트 서버로 데이터만 응답
-@CrossOrigin("http://localhost:3000/") // aws : 서버주소 변경
+@CrossOrigin("http://3.39.230.222:3000/") // aws : 서버주소 변경
 public class PlayerController {
 	
 	@Autowired
